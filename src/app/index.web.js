40d65cbf942e025db3c5b75f7b9ce4d96673fa91
Marcel <m1tinner@gmail.com>
2021-08-24 12:33:00 +0200

    add files

---
+++ src/app/index.web.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import { Router, navigate } from '@reach/router';
+
+import ScreenHome from '../components/screenHome';
+import ScreenMyPage from '../components/screenMyPage';
+import Menu from '../components/menu';
+import { themeStyles } from '../config';
+
+const App = () => {
+    useEffect(() => {
+        // Required for tizen
+        if (window.focus) window.focus();
+    }, []);
+
+    return (
+        <View style={themeStyles.app}>
+            <Menu focusKey="menu" navigate={navigate} />
+            <View style={themeStyles.appContainer}>
+                <Router>
+                    <ScreenHome path="/" />
+                    <ScreenMyPage path="my-page" />
+                </Router>
+            </View>
+        </View>
+    );
+};
+
+export default App;
